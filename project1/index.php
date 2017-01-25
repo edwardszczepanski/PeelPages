08bfb5810fa827719a80b37c698c1e64c14f8e98
edward <edward@cs.uoregon.edu>
2017-01-24 21:15:49 -0800

    Cleaning up concerns

--- project1/index.php
+++ project1/index.php
@@ -51,29 +51,13 @@
 			<form action="contacts.php" method="POST" id="sendForm" style="margin-top: 2%;" target="_blank">				
 				<input class="btn btn-success" type="submit" value="Open">
 				<button id="" type="button" class="btn btn-success">Export</button>				
+				<button id="closeAll" type="button" class="btn btn-success">Close All</button>				
 			</form>
           </div>
      </div>
      <div>
      </div>
  </body>
-  <script>
- $("input:checkbox").on('click', function() {
-  // in the handler, 'this' refers to the box clicked on
-  var $box = $(this);
-  if ($box.is(":checked")) {
-    // the name of the box is retrieved using the .attr() method
-    // as it is assumed and expected to be immutable
-    var group = "input:checkbox[name='" + $box.attr("name") + "']";
-    // the checked state of the group/box on the other hand will change
-    // and the current value is retrieved using .prop() method
-    $(group).prop("checked", false);
-    $box.prop("checked", true);
-  } else {
-    $box.prop("checked", false);
-  }
-});
-</script>
      <script src="./js/demo.js"></script>
      <script src="./js/bootstrap.min.js"></script>
  </html>
