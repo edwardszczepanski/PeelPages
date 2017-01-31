a6d3cbe8eb1de1400c51096858126407b5f1f260
HanWang <wang18@uoregon.edu>
2017-01-30 21:46:42 -0800

    formating login.php

--- project1/login.php
+++ project1/login.php
@@ -1,8 +1,4 @@
 <?php
-
-//if($_POST)
-//{
-  
 	include('connectionData.txt');
    
 	$mysqli = new mysqli($server, $user, $pass, $dbname, $port)
@@ -22,41 +18,25 @@
          <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
          <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
          <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
- </head>
+</head>
 
 <body>
 <div class="container" style="margin-left:auto; margin-right:auto;">
 
- <?php
+	<?php
 	$username=$_POST['username'];
-    $password=$_POST['password'];
-	
+	$password=$_POST['password'];
+
 	if(isset($username) && isset($password) && !empty($username) && !empty($password))
 	{
-		
-		//echo "SELECT * FROM peelPages.auth WHERE username ='".$username."' AND password='".$password."';</br>";
-		//echo "SELECT COUNT(*) FROM peelPages.auth WHERE username ='".$username."' AND password='".$password."';</br>";
-		//$stmt = $mysqli -> prepare("SELECT COUNT(*) FROM peelPages.auth WHERE username ='".$username."' AND password=".$password."';");
-		//$stmt = $mysqli -> prepare("SELECT COUNT(*) FROM peelPages.auth WHERE username ='"."han"."' AND password="."han"."';");
-		//echo "nidaye</br>";
+
 		$stmt = $mysqli -> prepare("SELECT * FROM auth WHERE username ='$username' AND password = '$password';");
-
 		$stmt->execute();
-		//echo "1nidaye</br>";
-
 		$userID=null;
-		//$username1=null;
-		//$password1=null;
 		$stmt->bind_result($userID,$username1,$password1);
-		//echo "caonima</br>";
-		//$stmt->bind_result($userID);
 		$stmt->store_result();
-
-		//printf("Number of rows: %d.\n", $stmt->num_rows);
-
 		while($stmt->fetch())printf('',$userID,$username1,$password1);	
-		//while($stmt->fetch())
-			//		printf('<h1>%s</h1>',$userID);	
+
 		if($stmt->num_rows==1)
 		{
 			session_start();
@@ -67,64 +47,36 @@
 			echo "<h2>Wrong username or password...</h2>";
 		}
 	}
-	
-/*	if($stmt->rowCount()==1)
-    {
-        session_start();
-        $_SESSION['auth']='true';
-        header('location:index.php');
-    }
-	*/
-	/*
-	echo $conn."<br>";
-	$username=$_POST['username'];
-    $password=$_POST['password'];
-    $sUser=mysqli_real_escape_string($conn,$username);
-    $sPass=mysqli_real_escape_string($conn,$password);
-    // For Security 
-    $query="SELECT * From peelPages.auth where username='$sUser' and password='$sPass'";
-        echo $query;
+	?>
 
-	$result=mysqli_query($conn,$query);
-    if(mysqli_num_rows($result)==1)
-    {
-        session_start();
-        $_SESSION['auth']='true';
-        header('location:index.php');
-    }*/
-//}
-
-?>
-
-<form style="margin-top:60px;" method="POST">
-<table>
-<tr>
-<td><p>Username: </p>
-</td>
-<td>    <input type="text" name="username">
-</td>
-</tr>
-
-<tr>
-<td><p>Password: </p>
-</td>
-<td>    <input type="password" name="password">
-
-</td>
-</tr>
-
-<tr>
-<td><p></p>
-</td>
-<td>    
-    <input class="btn btn-success" style="float:right;margin-top:5px;" type="submit">
-
-</td>
-</tr>
-</table>
-
-
-</form>
-</div>
+	<form style="margin-top:60px;" method="POST">
+		<table>
+		<tr>
+			<td>
+				<p>Username: </p>
+			</td>
+			<td>    
+				<input type="text" name="username">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<p>Password: </p>
+			</td>
+			<td>    
+				<input type="password" name="password">
+			</td>
+		</tr>
+			<tr>
+			<td>
+				<p></p>
+			</td>
+			<td>    
+				<input class="btn btn-success" style="float:right;margin-top:5px;" type="submit">
+			</td>
+		</tr>
+		</table>
+	</form>
+	</div>
  </body>
 </html>
